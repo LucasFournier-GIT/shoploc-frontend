1aeb6f43eefda2666dc23b2ed769aea037d1c496
camille <camillermpoirier@gmail.com>
2023-11-26 20:15:40 +0100

    Front for CreateAccount

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-// App.js
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './components/screens/LoginScreen';
 import CreateAccountScreen from './components/screens/CreateAccountScreen';
 import { StatusBar, View } from 'react-native';
+import { AppLoading } from 'expo';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+
   return (
 
       <NavigationContainer>
