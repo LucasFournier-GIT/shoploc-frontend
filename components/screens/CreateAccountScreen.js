748d23cd86128db76804ebdb7c6c2db48cf21646
camille <camillermpoirier@gmail.com>
2023-11-26 19:43:50 +0100

    Front for login page

---
+++ components/screens/CreateAccountScreen.js
@@ -0,0 +1,10 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const CreateAccountScreen = () => {
+    return (
+        <View >
+            <Text>Hello</Text>
+        </View>
+    )
+}
+export default CreateAccountScreen;
