b1d21cd03d91e70cac3380070725f053218a6c42
20014796 <lucas.fournier@adeo.com>
2023-11-27 10:51:30 +0100

    feat: ajout des fichiers de base - SCRUM-29

---
+++ components/Shop.js
@@ -0,0 +1,12 @@
+import { View } from "react-native/types";
+
+export default function Shop() {
+    return (
+        <View>
+            <StatusBar
+                animated={true}
+                backgroundColor="#5D3528"
+            />
+        </View>
+    );
+}
