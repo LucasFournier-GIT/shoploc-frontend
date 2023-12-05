a484bea094141de7339af943c2884c32e5d4b38f
camille <camillermpoirier@gmail.com>
2023-12-05 22:47:48 +0100

    feat: Liste des produits du magasin - SCRUM-30

---
+++ components/ProductCard.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+const ProductCard = ({ navigation, name, id, quantity, description, imageUrl }) => {
+  const [cartQuantity, setCartQuantity] = useState(0);
+
+  const handleAddToCart = () => {
+    setCartQuantity(cartQuantity + 1);
+  };
+
+  const handleRemoveFromCart = () => {
+    if (cartQuantity > 0) {
+      setCartQuantity(cartQuantity - 1);
+    }
+  };
+
+  return (
+    <View style={styles.card}>
+      <TouchableOpacity onPress={() => navigation.navigate('ProductDetails', { productId: id })}>
+        <Image source={{ uri: imageUrl }} style={styles.image} />
+        <Text style={styles.name}>{name}</Text>
+        <Text style={[styles.quantity, { color: quantity === 0 ? '#e74c3c' : '#2ecc71' }]}>
+          {quantity === 0 ? 'Rupture de stock' : `En stock: ${quantity}`}
+        </Text>
+        <Text style={styles.description}>{description}</Text>
+      </TouchableOpacity>
+
+      {quantity > 0 && (
+        <View style={styles.addToCart}>
+          <TouchableOpacity
+            onPress={handleRemoveFromCart}
+            style={[styles.button, cartQuantity === 0 && { display: 'none' }, styles.roundButton]}
+          >
+            <Text style={styles.buttonText}>-</Text>
+          </TouchableOpacity>
+          <Text style={[styles.cartQuantity, cartQuantity === 0 && { display: 'none' }]}>{cartQuantity}</Text>
+          <TouchableOpacity onPress={handleAddToCart} style={[styles.button, styles.roundButton]}>
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    borderColor: '#ccc',
+    borderRadius: 32.5,
+    backgroundColor: '#fff',
+    elevation: 5,
+    margin: 5,
+    width: '47%',
+    marginBottom: 5,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+    borderRadius: 32.5,
+    marginTop: 0,
+    marginBottom: 5,
+  },
+  name: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
+    paddingLeft: 5,
+    paddingRight: 5,
+  },
+  quantity: {
+    fontSize: 15,
+    fontStyle: 'italic',
+    textAlign: 'center',
+    paddingLeft: 5,
+    paddingRight: 5,
+  },
+  description: {
+    fontSize: 15,
+    fontStyle: 'italic',
+    color: '#555',
+    textAlign: 'center',
+    paddingLeft: 5,
+    paddingRight: 5,
+    marginBottom: 5,
+  },
+  addToCart: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    width: "35%", 
+    backgroundColor:"#275C50",
+    borderRadius: 50,
+
+  },
+  button: {
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    backgroundColor: '#2ecc71',
+    borderRadius: 5,
+    marginHorizontal: 5,
+    width:"100%",
+    alignItems:"center"
+  },
+  roundButton: {
+    borderRadius: 50,
+    backgroundColor: '#275C50',
+    elevation:10
+  },
+  buttonText: {
+    color: '#FFFFFF',
+  },
+  cartQuantity: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginHorizontal: 8,
+    color:"#ffffff"
+  },
+});
+
+export default ProductCard;
--- components/screens/ShopScreen.js
+++ components/screens/ShopScreen.js
@@ -1,28 +1,84 @@
-// Dans ShopScreen.js
-
 import React from 'react';
-import { View, Text } from 'react-native';
-import CustomSearchBar from './../CustomSearchBar'
-import CustomNavBar from './../CustomNavBar'
-import { StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import CustomSearchBar from './../CustomSearchBar';
+import CustomNavBar from './../CustomNavBar';
+import ProductCard from './../ProductCard';
+
+// Liste fictive de produits
+const dummyProducts = [
+  {
+    id: 1,
+    name: 'Produit 1',
+    quantity: 5,
+    description: 'Description du produit 1',
+    imageUrl: 'https://www.cuisinonsencouleurs.fr/wp-content/uploads/2012/03/DSC_0387k-1.jpg',
+  },
+  {
+    id: 2,
+    name: 'Produit 2',
+    quantity: 2,
+    description: 'Description du produit 2',
+    imageUrl: 'https://www.cuisinonsencouleurs.fr/wp-content/uploads/2012/03/DSC_0387k-1.jpg',
+  },
+  {
+    id: 3,
+    name: 'Produit 3',
+    quantity: 0,
+    description: 'Description du produit 3',
+    imageUrl: 'https://www.cuisinonsencouleurs.fr/wp-content/uploads/2012/03/DSC_0387k-1.jpg',
+  },
+  {
+    id: 4,
+    name: 'Produit 4',
+    quantity: 5,
+    description: 'Description du produit 4',
+    imageUrl: 'https://www.cuisinonsencouleurs.fr/wp-content/uploads/2012/03/DSC_0387k-1.jpg',
+  },
+];
 
 const ShopScreen = ({ route, navigation }) => {
   const { shopId } = route.params;
   return (
     <View style={styles.container}>
-        <CustomSearchBar></CustomSearchBar>
-        <CustomNavBar navigation={navigation}></CustomNavBar>
+      <Text style={styles.heading2}>Nom du shop n°{shopId}</Text>
+      <CustomSearchBar />
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        {dummyProducts.map((product) => (
+          <ProductCard
+            key={product.id}
+            name={product.name}
+            quantity={product.quantity}
+            description={product.description}
+            imageUrl={product.imageUrl}
+            navigation={navigation}
+            id={product.id}
+          />
+        ))}
+      </ScrollView>
+      <CustomNavBar navigation={navigation} />
     </View>
-    
   );
 };
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: "#EFEFEF",
-        height: "100%"
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#EFEFEF",
+    height: "100%"
+  },
+  scrollViewContent: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    paddingHorizontal: 5,
+    paddingBottom: "25%",
+    marginTop: 10,
+  },
+  heading2: {
+    color:'#5D3528',
+    fontSize: 26,
+    alignSelf:'center'
+  },
+});
 
 export default ShopScreen;
