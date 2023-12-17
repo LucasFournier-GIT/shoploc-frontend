fb9aa7c8fd2d6039b0388f801a03672c708a4c82
camille <camillermpoirier@gmail.com>
2023-12-17 17:27:31 +0100

    fix : position of navbar RecapCartSceen and PaymentScreen - SCRUM-37

--- components/screens/PaymentScreen.js
+++ components/screens/PaymentScreen.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { Image, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import CustomNavBar from '../CustomNavBar';
 import CustomInput from '../CustomInput';
 import CustomButton from '../CustomButton';
-import logo from './../../assets/logo.png';
-import colors from "./../../assets/colors";
+import colors from './../../assets/colors';
 import CustomModal from './../CustomModal';
 
 const PaymentScreen = ({ navigation, TotalAmount }) => {
@@ -18,66 +17,50 @@ const PaymentScreen = ({ navigation, TotalAmount }) => {
 
   const handleNumberChange = (value) => {
     setNumber(value);
-  }
+  };
 
   const handleValidate = () => {
-    //Retirer les éléments du panier
     setIsModalVisible(true);
+  };
 
-  }
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+    navigation.navigate('HomeScreen');
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.card}>
-
         <View style={styles.option}>
-            <TouchableOpacity style={styles.optionHead} onPress={() => handleOptionSelection('magasin')}>
+          <TouchableOpacity style={styles.optionHead} onPress={() => handleOptionSelection('magasin')}>
             <Text style={styles.optionText}>Payer en magasin</Text>
-            </TouchableOpacity>
-            {paymentOption === 'magasin' && <Text>Valider votre commande maintenant et payez la en magasin au moment de la retirer, sur présentation de votre code ShopLoc</Text>}
+          </TouchableOpacity>
+          {paymentOption === 'magasin' && (
+            <Text>Valider votre commande maintenant et payez-la en magasin au moment de la retirer, sur présentation de votre code ShopLoc</Text>
+          )}
         </View>
-
 
         <View style={styles.option}>
-
-            <TouchableOpacity style={styles.optionHead} onPress={() => handleOptionSelection('ligne')}>
+          <TouchableOpacity style={styles.optionHead} onPress={() => handleOptionSelection('ligne')}>
             <Text style={styles.optionText}>Payer en ligne</Text>
-            </TouchableOpacity>
-            {paymentOption === 'ligne' && 
-                <View>
-                    <Text>Payez votre commande en ligne dès maintenant ({TotalAmount}€)</Text>
-                    <CustomInput
-                        type={"text"}
-                        label={"Numéro de carte"}
-                        placeholder={"Votre numéro de carte"}
-                        onChange={handleNumberChange} 
-                    />
-                    <CustomInput
-                        type={"text"}
-                        label={"Date de péremption"}
-                        placeholder={"Date de fin de validité de la carte "}
-                        onChange={handleNumberChange} 
-                    />
-                    <CustomInput
-                        type={"text"}
-                        label={"CVC"}
-                        placeholder={"Cryptogramme"}
-                        onChange={handleNumberChange} 
-                    />
-                    <CustomButton text={"Valider"} onPress={handleValidate}></CustomButton>
-                </View>
-            }
+          </TouchableOpacity>
+          {paymentOption === 'ligne' && (
+            <View>
+              <Text>Payez votre commande en ligne dès maintenant ({TotalAmount}€)</Text>
+              <CustomInput type={'text'} label={'Numéro de carte'} placeholder={'Votre numéro de carte'} onChange={handleNumberChange} />
+              <CustomInput type={'text'} label={'Date de péremption'} placeholder={'Date de fin de validité de la carte '} onChange={handleNumberChange} />
+              <CustomInput type={'text'} label={'CVC'} placeholder={'Cryptogramme'} onChange={handleNumberChange} />
+              <CustomButton text={'Valider'} onPress={handleValidate} />
+            </View>
+          )}
         </View>
-
-
       </View>
-      <CustomModal
-        isVisible={isModalVisible}
-        onClose={() => setIsModalVisible(false)}
-        modalText={"Le paiement a bien été effectué"}
-      />
-      <CustomNavBar navigation={navigation} screen="CartScreen" />
 
+      <CustomModal isVisible={isModalVisible} onClose={handleCloseModal} modalText={'Le paiement a bien été effectué'} />
+
+      <View style={styles.bottomNavBar}>
+        <CustomNavBar navigation={navigation} screen="CartScreen" />
+      </View>
     </View>
   );
 };
@@ -89,52 +72,28 @@ const styles = StyleSheet.create({
   optionHead: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom:5,
+    marginBottom: 5,
   },
-  option:{
-    backgroundColor: colors.background ,
+  option: {
+    backgroundColor: colors.background,
     margin: 5,
     padding: 10,
   },
-  optionText:{
-    fontSize:17
+  optionText: {
+    fontSize: 17,
   },
   card: {
     backgroundColor: 'white',
     borderRadius: 32.5,
     padding: 20,
     elevation: 5,
-    margin:10,
+    margin: 10,
   },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+  bottomNavBar: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
   },
-  modalContent: {
-    backgroundColor: 'white',
-    borderRadius: 10,
-    padding: 20,
-    alignItems: 'center',
-    elevation: 5, // Ombre
-  },
-  modalText: {
-    fontSize: 18,
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  modalButton: {
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 10,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-    textAlign: 'center',
-  },
-
 });
 
 export default PaymentScreen;
--- components/screens/RecapCartScreen.js
+++ components/screens/RecapCartScreen.js
@@ -1,25 +1,23 @@
-import { ScrollView, StyleSheet, Text } from "react-native";
-import { View } from "react-native-animatable";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import CustomButton from "../CustomButton";
 import CustomNavBar from "../CustomNavBar";
 import colors from './../../assets/colors';
 
+const RecapCart = ({ navigation, route }) => {
+  const { TotalAmount } = route.params;
 
-// Calcul de la somme totale des produits (price * quantity)
-  const RecapCart = ({ navigation, route }) => {
-    const { TotalAmount } = route.params;
-  
-    const handleCancel = () => {
-      navigation.navigate("CartScreen");
-    }
+  const handleCancel = () => {
+    navigation.navigate("CartScreen");
+  }
 
-    const handleValidate = () => {
-      navigation.navigate("PaymentScreen", {navigation:navigation, TotalAmount:TotalAmount})
-    }
+  const handleValidate = () => {
+    navigation.navigate("PaymentScreen", { navigation: navigation, TotalAmount: TotalAmount });
+  }
 
-    return (
-      <View style = {styles.container}>
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
         <View style={styles.card}>
           <View>
             <Text style={styles.text}>Vous allez valider un panier d'un montant de <Text style={styles.amount}>{TotalAmount}€</Text>. Êtes-vous sûr de vouloir valider ce panier ? </Text>
@@ -29,41 +27,48 @@ import colors from './../../assets/colors';
             <CustomButton text={"Payer ce panier"} onPress={handleValidate}></CustomButton>
           </View>
         </View>
-        <CustomNavBar navigation={navigation} screen="CartScreen" />
       </View>
+      <CustomNavBar navigation={navigation} screen="CartScreen" style={styles.navBar} />
+    </View>
+  );
+};
 
-    );
-  };
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    card: {
-      backgroundColor: "white",
-      borderRadius: 32.5,
-      padding: 20,
-      elevation: 5,
-      maxWidth: 300, // Ajuster la largeur de la carte selon les besoins
-    },
-    amount: {
-      fontWeight: "bold",
-      color: colors.secondary,
-    },
-    text: {
-      textAlign: "center",
-      fontSize: 18,
-      marginBottom: 60,
-    },
-    row:{
-      position:"absolute",
-      bottom:0,
-      alignSelf:"center",
-      flexDirection:"row",
-      marginBottom:5,
-    }
-  });
-  
-  export default RecapCart;
-  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  card: {
+    backgroundColor: "white",
+    borderRadius: 32.5,
+    padding: 20,
+    elevation: 5,
+    maxWidth: "90%",
+  },
+  amount: {
+    fontWeight: "bold",
+    color: colors.secondary,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 18,
+    marginBottom: 60,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingBottom: 10,
+  },
+  navBar: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+  },
+});
+
+export default RecapCart;
