6fc5cb618fc4b4eac7f9faecc7a91bf071cecdd1
camille <camillermpoirier@gmail.com>
2024-03-30 21:25:08 +0100

    feat creation of Product cards for shop

--- components/shop/shopScreens/ShopProductsScreen.js
+++ components/shop/shopScreens/ShopProductsScreen.js
@@ -1,17 +1,61 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Image, StatusBar, View } from "react-native";
+import { Image, StatusBar, View, ScrollView } from "react-native"; // Importez ScrollView pour rendre la liste scrollable
 import { StyleSheet } from "react-native";
 import { Text } from "react-native";
 import { SearchBar } from 'react-native-elements';
-import { ScrollView } from "react-native";
 import logo from "./../../../assets/logo.png";
 import colors from "../../../assets/colors";
 import { AuthContext } from "../../AuthContext";
 import ShopNavbar from '../shopComponents/ShopNavbar';
-const ShopProductsScreen = ({ navigation }) => {
+import ProductCard from './../../ProductCard';
+import ShopProduct from "../shopComponents/ShopProduct";
 
+const ShopProductsScreen = ({ navigation }) => {
     const { token, updateToken } = useContext(AuthContext);
-    
+
+    //TODO recuperer le token du magasin 
+    //const [products, setProducts] = useState([]);
+
+    /*useEffect(() => {
+        // Fonction pour récupérer les produits du magasin
+        const fetchShopProducts = async () => {
+            try {
+                // Assurez-vous d'ajuster l'URL en fonction de votre API
+                const response = await fetch(`http://localhost:8080/api/product/shop/${token}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    // Mettez à jour l'état avec les produits récupérés
+                    setProducts(data.products);
+                } else {
+                    console.error('Erreur lors de la récupération des produits');
+                }
+            } catch (error) {
+                console.error('Erreur lors de la récupération des produits : ', error);
+            }
+        };
+
+        // Appelez la fonction pour récupérer les produits lorsque le composant est monté
+        fetchShopProducts();
+    }, [token]); // Assurez-vous de passer [token] comme dépendance pour que cette requête soit effectuée à chaque changement de token
+     */
+    const products = [
+        {
+            id: 1,
+            name: 'Product 1',
+            description: 'Description du produit 1',
+            imageUrl: 'https://i-sam.unimedias.fr/2023/02/09/istock-1222018207.jpg?auto=format%2Ccompress&crop=faces&cs=tinysrgb&fit=crop&h=501&w=890',
+            price: '$10.00',
+            quantity: 5,
+        },
+        {
+            id: 2,
+            name: 'Product 2',
+            description: 'Longue description pour le produit numero 2',
+            imageUrl: 'https://leblogdulait.fr/wp-content/uploads/2015/06/bouteils-de-lait.jpg',
+            price: '$15.00',
+            quantity: 0,
+        },
+    ];
 
     return (
         <View style={styles.container}>
@@ -21,10 +65,25 @@ const ShopProductsScreen = ({ navigation }) => {
             />
             <View style={styles.head} >
               <Image source={logo} style={styles.logo} />
-              <Text>Products</Text>
+              <Text style={styles.title}>Products</Text>
             </View>
             <ScrollView contentContainerStyle={styles.scrollViewContent}>
-                
+
+                <View>
+
+                    {products.map(product => (
+                        <ShopProduct
+                            key={product.id}
+                            navigation={navigation}
+                            imageUrl={product.imageUrl}
+                            name={product.name}
+                            quantity={product.quantity}
+                            price={product.price}
+                            description={product.description}
+                        />
+                    ))}
+                </View>
+
             </ScrollView>
             <ShopNavbar navigation={navigation} screen="ShopProductsScreen" />
         </View>
@@ -35,17 +94,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: colors.background,
-    },
-    searchBarContainer: {
-        backgroundColor: colors.background,
-        borderWidth: 1.5,
-        borderColor: colors.primary,
-        borderTopWidth: 0,
-        borderBottomWidth: 0,
-        paddingHorizontal: 10,
-        height: "150%",
-        flexDirection: "row",
-        flex: 1,
     },
     scrollViewContent: {
         flexDirection: 'row',
@@ -64,11 +112,18 @@ const styles = StyleSheet.create({
     head: {
         flexDirection: 'row',
         justifyContent: 'space-between',
+        alignItems: 'center',
         backgroundColor: colors.background,
         position: 'sticky',
         top: 0,
         zIndex: 1, 
-    }
+        padding: 10,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: colors.primary,
+    },
 });
 
 export default ShopProductsScreen;
